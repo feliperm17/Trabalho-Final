7fb0790cfb1ab9b115758d6f3224305c78ed5ebf
Moran <felipe.r.moran@gmail.com>
2024-08-28 13:57:10 -0300

    Junção das caracteristicas extraidas pelo lbp e lpq

--- Projeto/featureExtraction.py
+++ Projeto/featureExtraction.py
@@ -5,8 +5,83 @@ import numpy as np
 from skimage.io import imread
 from skimage import img_as_ubyte
 import matplotlib.pyplot as plt
+from scipy.signal import convolve2d
+from scipy.spatial.distance import cdist
 import os
-#import cv2 
+
+def lpq(img,winSize=7, decorr=1, mode='nh'):
+    rho=0.90
+
+    STFTalpha=1/winSize  # alpha in STFT approaches (for Gaussian derivative alpha=1)
+
+    convmode='valid' # Compute descriptor responses only on part that have full neigborhood. Use 'same' if all pixels are included (extrapolates np.image with zeros).
+
+    img=np.float64(img) # Convert np.image to double
+    r=(winSize-1)/2 # Get radius from window size
+    x=np.arange(-r,r+1)[np.newaxis] # Form spatial coordinates in window
+
+    #  STFT uniform window
+    #  Basic STFT filters
+    w0=np.ones_like(x)
+    w1=np.exp(-2*np.pi*x*STFTalpha*1j)
+    w2=np.conj(w1)
+
+    ## Run filters to compute the frequency response in the four points. Store np.real and np.imaginary parts separately
+    # Run first filter
+    filterResp1=convolve2d(convolve2d(img,w0.T,convmode),w1,convmode)
+    filterResp2=convolve2d(convolve2d(img,w1.T,convmode),w0,convmode)
+    filterResp3=convolve2d(convolve2d(img,w1.T,convmode),w1,convmode)
+    filterResp4=convolve2d(convolve2d(img,w1.T,convmode),w2,convmode)
+
+    # Initilize frequency domain matrix for four frequency coordinates (np.real and np.imaginary parts for each frequency).
+    freqResp=np.dstack([filterResp1.real, filterResp1.imag,
+                        filterResp2.real, filterResp2.imag,
+                        filterResp3.real, filterResp3.imag,
+                        filterResp4.real, filterResp4.imag])
+
+    if decorr == 1:
+        xp, yp = np.meshgrid(np.arange(1, winSize + 1), np.arange(1, winSize + 1))
+        pp = np.column_stack((yp.flatten(), xp.flatten()))
+        dd = cdist(pp, pp)
+        C = rho ** dd
+
+        q1 = w0.reshape((winSize,1))@w1.reshape((1,winSize))
+        q2 = w1.reshape((winSize,1))@w0.reshape((1,winSize))
+        q3 = w1.reshape((winSize,1))@w1.reshape((1,winSize))
+        q4 = w1.reshape((winSize,1))@w2.reshape((1,winSize))
+        
+        M = np.vstack((q1.real.T.ravel(), q1.imag.T.ravel(), q2.real.T.ravel(), q2.imag.T.ravel(),
+                       q3.real.T.ravel(), q3.imag.T.ravel(), q4.real.T.ravel(), q4.imag.T.ravel()))
+        
+        D = np.dot(M,C).dot(M.T)
+        A = np.diag([1.000007, 1.000006, 1.000005, 1.000004, 1.000003, 1.000002, 1.000001, 1])
+        U, S, V = np.linalg.svd(np.dot(A, D).dot(A))
+        V = V.T
+
+        freqRespShape = freqResp.shape
+        freqResp = freqResp.reshape((-1, freqResp.shape[2]))
+        freqResp = np.dot(V.T, freqResp.T).T
+        freqResp = freqResp.reshape(freqRespShape)
+        freqRespDecorr = freqResp.copy()    
+    
+    ## Perform quantization and compute LPQ codewords
+    inds = np.arange(freqResp.shape[2])[np.newaxis,np.newaxis,:]
+    LPQdesc=((freqResp>0)*(2**inds)).sum(2)
+
+    ## Switch format to uint8 if LPQ code np.image is required as output
+    if mode=='im':
+        LPQdesc=np.uint8(LPQdesc)
+
+    ## Histogram if needed
+    if mode=='nh' or mode=='h':
+        LPQdesc=np.histogram(LPQdesc.flatten(),range(257))[0]
+
+    ## Normalize histogram if needed
+    if mode=='nh':
+        LPQdesc=LPQdesc/LPQdesc.sum()
+
+    return LPQdesc
+
 
 def lbp(image : np.ndarray,
         P : int = 8,
@@ -21,102 +96,99 @@ def lbp(image : np.ndarray,
     return hist
 
 dir = './SoybeanSeeds/Broken'
-lbp_hist = []
+features_hist = []
 for i in range(51, 950):
-    img_path = os.path.join(dir, f'{i}.jpg')
-    gray_img = imread(img_path, as_gray=1)
-    gray_img_u = img_as_ubyte(gray_img)
-    hist : np.ndarray[float] = np.ndarray(1)
-    hist = lbp(gray_img_u,
-            P=8,
-            R=2,
-            method='nri_uniform')
-    hist = hist.astype("float")
-    hist /= (hist.sum()+ 1e-6)
-    hist2 : np.ndarray[float] = np.ndarray(1)
-    hist = np.insert(hist, 0, 1)
-
-    lbp_hist.append(hist)
+    img_path = os.path.join(dir, f'{i}.jpg')     # Completa o diretorio onde se encontra a imagem: /SoybeanSeeds/Broken/'i'.jpg
+    if os.path.exists(img_path):                 # Verifica se o arquivo existe
+        gray_img = imread(img_path, as_gray=1)   # Le a imagem e salva na variavel em escala de cinza    
+        gray_img_u = img_as_ubyte(gray_img)      # Converte a imagem pra uint8
+        hist_lbp = lbp(gray_img_u,               # Extração de caracteristicas lbp
+                P=8,
+                R=2,
+                method='nri_uniform')
+        hist_lbp = hist_lbp.astype("float")      # Converte para float
+        hist_lbp /= (hist_lbp.sum()+ 1e-6)       # Normaliza o histograma
+        hist_lbp = np.insert(hist_lbp, 0, 1)     # Adiciona a classe ao inicio do array
+        hist_lpq = lpq(gray_img, mode='nh')      # Extração de caracteristicas lpq
+        hist = np.concatenate((hist_lbp, hist_lpq)) # Concatena os dois histogramas de caracteristicas
+        features_hist.append(hist)                  # adiciona o histograma a tabela de caracteristicas
 
 
 dir = './SoybeanSeeds/Immature'
 for i in range(51, 1001):
-    img_path = os.path.join(dir, f'{i}.jpg')
-    gray_img = imread(img_path, as_gray=1)
-    gray_img_u = img_as_ubyte(gray_img)
-    hist : np.ndarray[float] = np.ndarray(1)
-    hist = lbp(gray_img_u,
-            P=8,
-            R=2,
-            method='nri_uniform')
-    hist = hist.astype("float")
-    hist /= (hist.sum()+ 1e-6)
-    hist2 : np.ndarray[float] = np.ndarray(1)
-    hist = np.insert(hist, 0, 2)
-
-    lbp_hist.append(hist)
+    img_path = os.path.join(dir, f'{i}.jpg') 
+    if os.path.exists(img_path):   
+        gray_img = imread(img_path, as_gray=1)
+        gray_img_u = img_as_ubyte(gray_img)
+        hist_lbp = lbp(gray_img_u,
+                P=8,
+                R=2,
+                method='nri_uniform')
+        hist_lbp = hist_lbp.astype("float")
+        hist_lbp /= (hist_lbp.sum()+ 1e-6)
+        hist_lbp = np.insert(hist_lbp, 0, 2) 
+        hist_lpq = lpq(gray_img, mode='nh')
+        hist = np.concatenate((hist_lbp, hist_lpq))
+        features_hist.append(hist)
 
 
 dir = './SoybeanSeeds/Skin-damaged'
 for i in range(51, 1001):
-    img_path = os.path.join(dir, f'{i}.jpg')
-    gray_img = imread(img_path, as_gray=1)
-    gray_img_u = img_as_ubyte(gray_img)
-    hist : np.ndarray[float] = np.ndarray(1)
-    hist = lbp(gray_img_u,
-            P=8,
-            R=2,
-            method='nri_uniform')
-    hist = hist.astype("float")
-    hist /= (hist.sum()+ 1e-6)
-    hist2 : np.ndarray[float] = np.ndarray(1)
-    hist = np.insert(hist, 0, 3)
+    img_path = os.path.join(dir, f'{i}.jpg') 
+    if os.path.exists(img_path):   
+        gray_img = imread(img_path, as_gray=1)
+        gray_img_u = img_as_ubyte(gray_img)
+        hist_lbp = lbp(gray_img_u,
+                P=8,
+                R=2,
+                method='nri_uniform')
+        hist_lbp = hist_lbp.astype("float")
+        hist_lbp /= (hist_lbp.sum()+ 1e-6)
+        hist_lbp = np.insert(hist_lbp, 0, 2) 
+        hist_lpq = lpq(gray_img, mode='nh')
+        hist = np.concatenate((hist_lbp, hist_lpq))
+        features_hist.append(hist)
 
-    lbp_hist.append(hist)
 
 dir = './SoybeanSeeds/Spotted'
 
 for i in range(51, 1001):
-    img_path = os.path.join(dir, f'{i}.jpg')
-    gray_img = imread(img_path, as_gray=1)
-    gray_img_u = img_as_ubyte(gray_img)
-    hist : np.ndarray[float] = np.ndarray(1)
-    hist = lbp(gray_img_u,
-            P=8,
-            R=2,
-            method='nri_uniform')
-    hist = hist.astype("float")
-    hist /= (hist.sum()+ 1e-6)
-    hist2 : np.ndarray[float] = np.ndarray(1)
-    hist = np.insert(hist, 0, 4)
+    img_path = os.path.join(dir, f'{i}.jpg') 
+    if os.path.exists(img_path):   
+        gray_img = imread(img_path, as_gray=1)
+        gray_img_u = img_as_ubyte(gray_img)
+        hist_lbp = lbp(gray_img_u,
+                P=8,
+                R=2,
+                method='nri_uniform')
+        hist_lbp = hist_lbp.astype("float")
+        hist_lbp /= (hist_lbp.sum()+ 1e-6)
+        hist_lbp = np.insert(hist_lbp, 0, 2) 
+        hist_lpq = lpq(gray_img, mode='nh')
+        hist = np.concatenate((hist_lbp, hist_lpq))
+        features_hist.append(hist)
 
-    lbp_hist.append(hist)
 
 dir = './SoybeanSeeds/Intact'
 for i in range(51, 1001):
-    img_path = os.path.join(dir, f'{i}.jpg')
-    gray_img = imread(img_path, as_gray=1)
-    gray_img_u = img_as_ubyte(gray_img)
-    hist : np.ndarray[float] = np.ndarray(1)
-    hist = lbp(gray_img_u,
-            P=8,
-            R=2,
-            method='nri_uniform')
-    hist = hist.astype("float")
-    hist /= (hist.sum()+ 1e-6)
-    hist2 : np.ndarray[float] = np.ndarray(1)
-    hist = np.insert(hist, 0, 5)
-
-    lbp_hist.append(hist)
-
-
-df_lbp_hist_b = pd.DataFrame(lbp_hist)
-
-df_lbp_hist_b.to_csv('./features.csv', index=False)
+    img_path = os.path.join(dir, f'{i}.jpg') 
+    if os.path.exists(img_path):   
+        gray_img = imread(img_path, as_gray=1)
+        gray_img_u = img_as_ubyte(gray_img)
+        hist_lbp = lbp(gray_img_u,
+                P=8,
+                R=2,
+                method='nri_uniform')
+        hist_lbp = hist_lbp.astype("float")
+        hist_lbp /= (hist_lbp.sum()+ 1e-6)
+        hist_lbp = np.insert(hist_lbp, 0, 2) 
+        hist_lpq = lpq(gray_img, mode='nh')
+        hist = np.concatenate((hist_lbp, hist_lpq))
+        features_hist.append(hist)
 
 
-print(df_lbp_hist_b)
+df_features_hist = pd.DataFrame(features_hist)
 
+df_features_hist.to_csv('./features.csv', index=False)
 
-#colored_img = cv2.imread('./SoybeanSeeds/Broken/1.jpg')
-#cv2.imshow(colored_img)
+print(df_features_hist)
